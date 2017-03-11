ff07bb34301b8d613d5c7a86aae394b199740d65
Derek Smart <derek@grindaga.com>
2017-03-10 19:55:22 -0500

    configuration files and setup scripts
    Signed-off-by: Derek Smart <derek@payscape.com>

---
+++ ansible/utilities/init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Helper script to get started with setting up Bash mode and loading keys
+# Do not run this directly but 'source' it from your script
+#
+
+set -u # Variables must be explicit
+set -e # If any command fails, fail the whole thing
+set -o pipefail
+
+chmod 0600 inventory/grindaga.pem
+eval `ssh-agent -s`
+ssh-add inventory/grindaga.pem
+source inventory/aws_keys
+
+# Update the cache
+inventory/ec2.py --refresh-cache >/dev/null
